02c00e7a366d0523f0d3738cfb28de4d304ccd7f
KevinWang <kevin@simsix.com>
2018-07-14 01:38:36 +0800

    Finish.

---
+++ Homework1/Controllers/MoneyController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Homework1.Models.ViewModels;
+
+namespace Homework1.Controllers
+{
+    public class MoneyController : Controller
+    {
+        // GET: Money
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult GetMoneyList()
+        {
+            var moneyList = new List<MoneyViewModels>();
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < 50; i++)
+            {
+                moneyList.Add(new MoneyViewModels
+                {
+                    type = (rnd.Next() % 2 == 0 ? "支出" : "收入"),
+                    recordDate = DateTime.Now.AddDays(-i),
+                    amount = rnd.Next(0, short.MaxValue)
+                });
+            }
+
+
+            return View(moneyList);
+        }
+    }
+}
---
+++ Homework1/Models/ViewModels/MoneyViewModels.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Homework1.Models.ViewModels
+{
+    public class MoneyViewModels
+    {
+        [Display(Name = "類別")]
+        public string type { get; set; }
+
+        [Display(Name = "日期")]
+        public DateTime recordDate { get; set; }
+
+        [Display(Name = "金額")]
+        public int amount { get; set; }
+
+        [Display(Name = "備註")]
+        public string remark { get; set; }
+    }
+}
